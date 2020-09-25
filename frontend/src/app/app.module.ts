e6c2e77b862bdf7b3d76f6b4b6acdcfca3d90ca7
Marcelo Rodrigues Gonzaga <marcelo.gonzaga@presidencia.gov.br>
2020-09-25 08:57:43 -0300

    Add frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./components/template/header/header.component";
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { NavComponent } from "./components/template/nav/nav.component";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
+import { HomeComponent } from "./views/home/home.component";
+import { ProductsComponent } from "./views/products/products.component";
+import { RedDirective } from "./directives/red.directive";
+import { ForDirective } from "./directives/for.directive";
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { HttpClientModule } from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductsComponent,
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Product } from "../product.model";
+import { ProductService } from "../product.service";
+
+@Component({
+  selector: "app-product-create",
+  templateUrl: "./product-create.component.html",
+  styleUrls: ["./product-create.component.css"],
+})
+export class ProductCreateComponent implements OnInit {
+  product: Product = {
+    name: "Produto de teste",
+    price: 125.98,
+  };
+
+  constructor(private productService: ProductService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Operação executada com sucesso");
+      this.router.navigateByUrl("products");
+    });
+  }
+
+  cancel(): void {
+    this.router.navigateByUrl("products");
+  }
+}
